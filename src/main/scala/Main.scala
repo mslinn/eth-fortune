b4a2defc0546d68108866131f63b2a9abfb4f07c
Mike Slinn <mslinn@micronauticsresearch.com>
2017-11-13 15:15:55 -0800

    -

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,10 +3,12 @@ import com.mchange.sc.v1.consuela.ethereum.stub.Generator
 
 object Main extends App {
   val abi = Abi("") // todo recommended way to run solc, in order to create ABI?
-  val x = Generator.generateContractStub(
+  val (className, code) = Generator.generateContractStub(
     baseClassName = "Fortune",
     abi = abi,
     async = true,
     fullyQualifiedPackageName = "com.mchange.test"
   )
+
+  // todo how to turn the code into something executable?
 }
